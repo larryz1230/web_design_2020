5c2670334928c1cc4f706d7b02ad754a4927e5c0
larryz1230 <larryzhi1230@gmail.com>
2020-09-18 10:04:52 -0700

    9/18

--- tags_project/script/script.js
+++ tags_project/script/script.js
@@ -1,9 +1,25 @@
 function onMapClick(loc){
 	var textbox = document.getElementById('text');
+	var tempString = "The continent you selected was "
 	if (loc=="NA"){
-		textbox.innerHTML = "North America";
-	} else if (loc="SA"){
-		textbox.innerHTML = "South America"
+		tempString += "North America";
+	}  else if (loc=="AF"){
+		tempString += "Africa";
+	}else if (loc=="SA"){
+		tempString += "South America";
+	} else if (loc=="EU"){
+		tempString += "Europe";
+	} else if (loc=="AS"){
+		tempString += "Asia";
+	} else if (loc=="AU"){
+		tempString += "Australia";
 	}
-	document.getElementById("toast").style.visibility = "visible"
+	textbox.innerHTML = tempString;
+	// document.getElementById("toast").style.visibility = "visible"
+	return;
+}
+
+function resetselection(){
+	var textbox = document.getElementById('text');
+	textbox.innerHTML = "Select a continent";
 }
